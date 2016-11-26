bcd54dc02795a0a6d9b68b7e6213e5dd206f4391
xime <ximebcn@yahoo.es>
2016-11-26 21:10:26 +0100

    Configuring an autoloader and testing that phpunit works

---
+++ tests/TestingPhpunitWorks.php
@@ -0,0 +1,14 @@
+<?php
+namespace Marsrover\test;
+
+use PHPUnit\Framework\TestCase;
+
+class TestingPhpunitWorks extends TestCase
+{
+    public function testItWorks()
+    {
+        $this->assertEquals(true,true);
+    }
+}
+
+
